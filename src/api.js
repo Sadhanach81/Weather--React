ee6380b5a25bcb922abf3ec290f8c47405d9beb2
Sadhanach81 <105736483+Sadhanach81@users.noreply.github.com>
2024-04-11 11:19:31 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "de428c1308msh04c8632daab9c18p1db39fjsnabbfcfa1e441",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "1061fb13a9d5311dfc6374d1c0a62880"; // enter your key from openweather API
